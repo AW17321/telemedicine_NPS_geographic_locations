ece0183d39748cb546065667e71a34f536cde203
AW17321 <73318413+AW17321@users.noreply.github.com>
2024-06-25 08:01:41 -0700

    Server list and powershell script that pings the list.
    The server list contains servers to ping from iperf servers. The PowerShell script goes through each server, pings it and records the response times. There is also a loop that allows the script to ping the whole list however many times you would like.

---
+++ test.ps1
@@ -0,0 +1,23 @@
+$Output= @()
+$names = Get-content "servers.txt"
+
+for ($i = 0;  $i -lt 1; $i++){# Make sure loop starts from 0 and goes to number of trials.
+	#Write-Host $i #debug, write number of loop on terminal
+	#Write-Host (Get-Date).ToString('MM/dd/yyyy hh:mm:ss tt') #time per loop
+	foreach ($name in $names){ #loop down the list of servers
+
+		$obj = Test-Connection -Count 1 -Delay 1 -BufferSize 1500 -ComputerName $name -ErrorAction SilentlyContinue -AsJob |Receive-Job -Wait
+
+		if($obj){
+   			$Output+= ($obj | Select -ExpandProperty "Responsetime")
+   			Write-Host ($obj | Select -ExpandProperty "Responsetime") #debug, write on terminal
+		}
+  		else{
+   			$Output+= "0"
+    			Write-Host "0" #debug, write on terminal
+  		}
+
+	}
+}
+$Output | Out-file "result.csv"
+Pause

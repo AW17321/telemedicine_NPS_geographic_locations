d3272f8f4ff25292aaf58f05c9c96788c4eecb53
AW17321 <73318413+AW17321@users.noreply.github.com>
2024-06-25 15:04:01 -0700

    Update Sequential_Test.ps1

--- Sequential_Test.ps1
+++ Sequential_Test.ps1
@@ -4,7 +4,7 @@ $names = Get-content "servers.txt"
 
 for ($i = 0;  $i -lt 750; $i++){# Make sure loop starts from 0 and goes to number of trials.
 	#Write-Host $i #debug, write number of loop on terminal
-	#Write-Host (Get-Date).ToString('MM/dd/yyyy hh:mm:ss tt') #time per loop
+	Write-Host (Get-Date).ToString('MM/dd/yyyy hh:mm:ss tt') #time per loop
 	foreach ($name in $names){ #loop down the list of servers
 
 		$obj = Test-Connection -Count 1 -Delay 1 -BufferSize 1500 -ComputerName $name -ErrorAction SilentlyContinue

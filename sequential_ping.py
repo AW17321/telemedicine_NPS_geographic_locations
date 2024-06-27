11243d19439f818d4666f0f67fe9575d97e0977c
AW17321 <73318413+AW17321@users.noreply.github.com>
2024-06-27 10:23:42 -0700

    Update sequential_ping.py

--- sequential_ping.py
+++ sequential_ping.py
@@ -6,7 +6,7 @@ pip3 install ping3
 Linux Ubuntu, using VS on Anaconda:
 python3 -m pip install ping3
 '''
-#Press Ctrl+C to end script when you feel like it.
+#Press Ctrl+C to end script when you feel like it. It should save to CSV
 
 from tkinter.filedialog import askopenfilename
 from ping3 import ping #pinging stuff
@@ -15,10 +15,10 @@ import time
 import csv
 
 # opening the file with w+ mode truncates the file
-with open(r"C:\Users\aaron\OneDrive - Naval Postgraduate School\NPS Research\Powershell script\ServerPingOutput.CSV", "w+") as output_file:
+with open(r"ServerPingOutput.CSV", "w+") as output_file:
     output = csv.writer(output_file) 
     while (1):
-        with open(r"C:\Users\aaron\OneDrive - Naval Postgraduate School\NPS Research\Powershell script\servers.csv") as csv_file:
+        with open(r"servers.csv") as csv_file:
             csv_reader = csv.reader(csv_file)
             for name in csv_reader:
                 print(name[0]) #check server list

c73657b668834c8b901bc64d59651e1ff5c9cad9
AW17321 <73318413+AW17321@users.noreply.github.com>
2024-06-27 10:12:13 -0700

    Update sequential_ping.py
    Using a keyboard interrupt instead of a timer.

--- sequential_ping.py
+++ sequential_ping.py
@@ -1,20 +1,23 @@
 '''
-Using python 3.7
+Using python 3.9
 
 Windows:
 pip3 install ping3
+Linux Ubuntu, using VS on Anaconda:
+python3 -m pip install ping3
 '''
+#Press Ctrl+C to end script when you feel like it.
+
 from tkinter.filedialog import askopenfilename
 from ping3 import ping #pinging stuff
 
 import time
 import csv
-starttime = time.time()
 
 # opening the file with w+ mode truncates the file
 with open(r"C:\Users\aaron\OneDrive - Naval Postgraduate School\NPS Research\Powershell script\ServerPingOutput.CSV", "w+") as output_file:
     output = csv.writer(output_file) 
-    while (time.time()<starttime+90000):#run for 25 hours
+    while (1):
         with open(r"C:\Users\aaron\OneDrive - Naval Postgraduate School\NPS Research\Powershell script\servers.csv") as csv_file:
             csv_reader = csv.reader(csv_file)
             for name in csv_reader:
